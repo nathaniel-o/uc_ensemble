568bb8858382f75a6433023f9bd074f310fa4eb9
Nathaniel <nathanielotoole@gmail.com>
2025-07-20 12:20:01 -0400

    Rm Extra Plugins

--- plugins/crowdsignal-forms/build/apifetch.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'wp-polyfill'), 'version' => '40bf258c60a45cc06a1b');
--- plugins/crowdsignal-forms/build/vote.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-blocks', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '99b0735e48b8549df1a7');
