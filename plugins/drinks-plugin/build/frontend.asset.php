a207a6da7ed7e5657a427d93c394ddbc0b32b0a8
Nathaniel <nathanielotoole@gmail.com>
2025-09-01 15:35:16 -0400

    Fix it

---
+++ plugins/drinks-plugin/build/frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '7a49e02256651d7792b4');
---
+++ plugins/drinks-plugin/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'ac00e89b5ff514da6ea5');
