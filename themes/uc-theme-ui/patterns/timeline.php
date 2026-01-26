38dfdf940739d637bae82ade9cc21cb10fdad819
Nathaniel OToole <nathanielotoole@gmail.com>
2026-01-26 16:02:41 -0500

    timeline

--- themes/uc-theme-ui/patterns/timeline.php
+++ themes/uc-theme-ui/patterns/timeline.php
@@ -13,63 +13,70 @@
 					<p id="under-h3"> ( UC . com) </p>
  
 			<div class="ruler">
-
+						<!-- 2 -->
 						<div class = "horlage right">
 						<div class = "content">
-							<h5 class="h5">March 2023</h5>
-							<p class = "memory">UC.com goes LIVE! Visitors can enjoy a gallery of Fireplace, Everyday, Special Occasion, Romantic, and Seasonal Cocktails; search ingredients, glasses, garnishes, and other elements; and Contact Us for more information.</p>
+							<h5 class="h5">2025</h5>
+							<p class = "memory">Beta testing begins on the desktop site, with subsequent changes to coding, additional plug-ins, and several revisions of visual presentation</p>
 						</div>
 						</div>
 						
+						<!-- 1 -->
 						<div class = "horlage left">
 						<div class = "content">
-							<h5 class="h5">January 2023</h5>
-							<p class = "memory">The About Us page is complete, and will allow visitors to the site to contact us.</p>
+							<h5 class="h5">2026</h5>
+							<p class = "memory">Beta testing is performed the mobile site, and then UntouchedCocktails.com goes LIVE! Visitors can enjoy a gallery of Fireplace, Everyday, Special Occasion, Romantic, and Seasonal Cocktails; search ingredients, glasses, garnishes, and other elements; and Contact Us for more information.</p>
 						</div>
 						</div>
 						
+						<!-- 4 -->
 						<div class = "horlage right" id="right2">
 						<div class = "content">
-							<h5 class="h5">December 2022</h5>
-							<p class = "memory">The About Us page is complete, and will allow visitors to the site to contact us.</p>
+							<h5 class="h5">Fall 2023</h5>
+							<p class = "memory">Nathaniel introduces the more complex scripting aspects of the site’s functionality. We continue to build the library, and using a defined set of parameters, tag photos so that visitors to the website may search by elements seen in the pictures.</p>
 						</div>
 						</div>
 						
+						<!-- 3 --> 
 						<div class = "horlage left" id = "left2">
 						<div class = "content">
-							<h5 class="h5">September 2022</h5>
-							<p class = "memory"> The Home Page is both beautiful and functional! A systematic labelling of searchable terms is applied to all of the photos in the library. We experiment with new photo environments.
+							<h5 class="h5">2024</h5>
+							<p class = "memory"> Overhaul of the color schematics for the website pages, so that search results assist users in exploring the gallery more organically. Nathaniel incorporates new functionality using existing plug-ins as well as writing his own, thereby further customizing the user’s experience on the site. Meanwhile, Julia starts an in-person cocktail experience business (Clear Ice &amp; Bitters) that will become a source of additional and new images to add to the gallery, maintaining the key principle that photos used for UntouchedCocktails.com are unedited or filtered, taken before a first sip.
 							</p>
 						</div>
 						</div>
 						
+						<!-- 6 -->
 						<div class = "horlage right" id="right3">
 						<div class = "content">
-							<h5 class="h5">August 2022</h5>
-							<p class = "memory">The composition of our site is finalized, using cocktail categories as an organizing structure, and including site details such as color, shapes, and layout. We begin to explore new and different glass profiles to enrich the visual variety of cocktails.
+							<h5 class="h5">2022</h5>
+							<p class = "memory">Nathaniel and Julia discuss the possibility of building a gallery-based website where the photos can be publicly shared. We decide upon a visually-driven philosophy, approaching cocktails as works of art, rather than recipes. We establish parameters and begin sketching out what the website might look like. Logo design is complete!  athaniel begins the website build, researching coding and scripting. Our existing library of (literally) thousands of photos is sorted, vetted, and catalogued. Meanwhile, the library continues to grow					</p>
+						</div>
+						</div>
+						
+						<!-- 5 -->
+						<div class = "horlage left" id = "left3">
+						<div class = "content">
+							<h5 class="h5">Spring 2023</h5>
+							<p class = "memory">The composition of our site is finalized, using cocktail categories as an organizing structure, and including site details such as color, shapes, and layout. We begin to explore new and different glass profiles to enrich the visual variety of cocktails. The Home Page is both beautiful and functional! A systematic labelling of searchable terms is applied to all of the photos in the library. We experiment with new photo environments.</p>
+						</div>
+						</div>
+						
+						<!-- 8 -->
+						<div class = "horlage right" id="right4">
+						<div class = "content">
+							<h5 class="h5">2020</h5>
+							<p class = "memory">COVID-19 lockdown inspires more creativity, resulting in more attention on the physical appearance (in addition to taste profile) of each cocktail. Julia joins Bob in taking and keeping a visual library of cocktails, and on average, three or four cocktails (with 8-10 pictures per cocktail) are captured each week. 
 							</p>
 						</div>
 						</div>
 						
-						<div class = "horlage left" id = "left3">
-						<div class = "content">
-							<h5 class="h5">July 2022</h5>
-							<p class = "memory">Our existing library of (literally) thousands of photos is sorted, vetted, and catalogued. Meanwhile, the library continues to grow.</p>
-						</div>
-						</div>
-						
-						<div class = "horlage right" id="right4">
-						<div class = "content">
-							<h5 class="h5">June 2022</h5>
-							<p class = "memory">Nathaniel begins the website build, researching coding and scripting. </p>
-						</div>
-						</div>
-						
-						
+						<!-- 7 -->
 						<div class = "horlage left" id = "left4">
 						<div class = "content">
-							<h5 class="h5">May 2022</h5>
-							<p class = "memory">We nail down the philosophy and purpose behind the project, and begin sketching out what a website might look like. Logo design is complete!</p>
+							<h5 class="h5">2021</h5>
+							<p class = "memory">	Experimentation with botanicals, glass shapes, and garnishes. But what are we going to DO with all of these photos?!					
+							</p>
 						</div>
 						</div>
 						<div class = "horlage right" id="right5">
@@ -79,53 +86,43 @@
 						</div>
 						</div>
 						
+						<!-- 10 -->
 						<div class = "horlage left" id = "left5">
 						<div class = "content">
-							<h5 class="h5">Winter 2020-21</h5>
-							<p class = "memory">Garnishes! And what are we going to DO with all of these photos?!</p>
-						</div>
+							<h5 class="h5">2015</h5>
+							<p class = "memory">			Julia begins to explore new types of liquors and mixers, experimenting with colors and garnishes as part of the cocktail experience.			</div>
+							</p>
 						</div>
 						
+						<!-- 9 -->
 						<div class = "horlage right" id="right6">
 						<div class = "content">
-							<h5 class="h5">May 2020</h5>
-							<p class = "memory">Julia joins Bob in taking and keeping a visual library of cocktails, and on average, three or four cocktails (with 8-10 pictures per cocktail) are captured each week.</p>
+							<h5 class="h5">2017</h5>
+							<p class = "memory">Bob begins taking photos of cocktails – the beginning of our comprehensive
+library. The images motivate research into layering, liquid density, infusion, and
+other mixing techniques; the spectrum of possibilities grows exponentially!</p>
 						</div>
 						</div>
 						
+						<!-- 12 
 						<div class = "horlage left" id = "left6">
 						<div class = "content">
 							<h5 class="h5">March 2020</h5>
 							<p class = "memory">COVID-19 lockdown inspires more creativity, resulting in more attention on the physical appearance (in addition to taste profile) of each cocktail. </p>
 						</div>
-						</div>
+						</div-->
 						
+						<!-- 11 -->
 						<div class = "horlage right" id="right7">
 						<div class = "content">
-							<h5 class="h5">Autumn 2017</h5>
-							<p class = "memory">The images motivate research into layering, liquid density, infusion, and other mixing techniques; the spectrum of possibilities grows exponentially!</p>
+							<h5 class="h5">2014</h5>
+							<p class = "memory">After years of enjoying cocktails crafted at the kitchen counter, Julia’s husband
+(Bob) decides to build her a professional-grade bar, using blended woodgrains
+and finished with an exquisite curved moulding.</p>
 						</div>
 						</div>
 						
-						<div class = "horlage left" id = "left7">
-						<div class = "content">
-							<h5 class="h5">July 2017</h5>
-							<p class = "memory">Bob begins taking photos of cocktails – the beginning of our comprehensive library.</p>
-						</div>
-						</div>
 						
-						<div class = "horlage right" id="right8">
-						<div class = "content">
-							<h5 class="h5">2015</h5>
-							<p class = "memory">Julia begins to explore new types of liquors and mixers, experimenting with colors and garnishes as part of the cocktail experience.</p>
-						</div>
-						</div>
-						
-						<div class = "horlage left" id = "left8">
-						<div class = "content">
-							<h5 class="h5">October 2014</h5>
-							<p class = "memory">After years of enjoying cocktails crafted at the kitchen counter, Julia’s husband (Bob) decides to build her a professional-grade bar, using blended woodgrains and finished with an exquisite curved moulding.  </p>
-						</div>
 						</div>
 							
 						  
