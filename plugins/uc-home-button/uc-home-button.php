ecf966548641ade9e5800da16ca05b938c3dd46a
Nathaniel <nathanielotoole@gmail.com>
2026-05-09 11:52:32 -0400

    untouched-cocktails/uc-home-button

--- plugins/uc-home-button/uc-home-button.php
+++ plugins/uc-home-button/uc-home-button.php
@@ -8,7 +8,7 @@
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       uc-home-button
+ * Text Domain:       untouched-cocktails/uc-home-button
  *
  * @package CreateBlock
  */
