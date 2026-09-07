7b329615a45e1144530afcd32cfd7293267d27fe
Nathaniel OToole <nathanielotoole@gmail.com>
2026-09-07 09:17:19 -0400

    roughly island flow biography-single.php

--- themes/uc-theme-ui/patterns/biography-single.php
+++ themes/uc-theme-ui/patterns/biography-single.php
@@ -8,6 +8,7 @@
 ?>
 <!-- wp:html -->
 <div class="uc-biography-single">
+<div class="frame">
 <div class="wrap">
 	<div class="col1">
 		<img class="img" src="https://untouchedcocktails.com/wp-content/uploads/2026/02/headshotJRA-1.jpg" width="160" height="160" alt="Julia Adams">
@@ -21,6 +22,23 @@
 			<p>Pleasing the palate is always top priority, of course, but in discovering the contribution the visual makes to taste, she and her husband had fun taking pictures of their cocktails as they were served. What started as a whim has become a sizable collection of “raw” images - an artistic gallery of cocktails. In addition to capturing the visual beauty of cocktails, Julia is also the founder and owner of Clear Ice &amp; Bitters, providing an in-home, intimate cocktail experience.</p>
 		</div>
 	</div>
+</div>
+</div>
+<div class="frame">
+<div class="wrap">
+	<div class="col1">
+		<img class="img" src="https://untouchedcocktails.com/wp-content/uploads/2026/02/000a.jpg" width="160" height="160" alt="Nathaniel OToole">
+		<div class="inner">
+			<p>Intrigued by the social implications of the digital age, Nathaniel's interest for this project lies in highlighting simple pleasures' convergence with artistic inspiration, for all to enjoy. A hobby student, he maintains that self-education projects have been beneficial in maintaining a user-minded approach to IT.</p>
+		</div>
+	</div>
+	<div class="col2">
+		<div class="img"></div>
+		<div class="inner">
+			<p>UntouchedCocktails.com has proven a wonderful learning platform and enticing vision to bring to life. We hope the community will continue to share their passions for the finer things, both simple and elegant, through the Untouched Image - where every moment captured shared represents a love for serving, for refinement, and for time well spent. May our collection grow with every Celebration!</p>
+		</div>
+	</div>
+</div>
 </div>
 </div>
 <!-- /wp:html -->
